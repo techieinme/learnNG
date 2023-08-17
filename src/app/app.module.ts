d294d07d4c3cd8fe0482ad19204c884511c264b6
Lakshmikanth <lakshmikantharaju09@gmail.com>
2023-08-17 14:12:19 +0530

    content projection

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ParentComponent } from './parent/parent.component';
+import { ChildrenComponent } from './children/children.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ParentComponent,
+    ChildrenComponent
   ],
   imports: [
     BrowserModule,
